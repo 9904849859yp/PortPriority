4c894c804b0aa2b917c5706ce9b786ba04ac75c6
Yash <msptest101@gmail.com>
2022-06-03 13:19:05 +0530

    first commit

---
+++ js/custome.js
@@ -0,0 +1,14 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+        $('header').addClass("sticky");
+    }
+    else {
+        $('header').removeClass("sticky");
+    }
+});
+
+
+// Animation
+
+
+AOS.init();
